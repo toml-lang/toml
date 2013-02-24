e397a4228b688e13cb6c01060cfc40ca8f3a7404
Charlie Somerville <charlie@charliesomerville.com>
2013-02-24 16:22:12 +1100

    optimize more

--- lib/toml.rb
+++ lib/toml.rb
@@ -6,43 +6,38 @@ module TOML
     raise NotImplementedError, "whoops"
   end
 
-  class LoadContext
+  def self.load(__toml)
+    __b = binding
+    __b.eval(to_ruby(__toml))
+    hashize(binding_vars(__b))
+  end
+
+  class << self
+  private
     def _section(mergee)
       b = nil
       c = Object.new
       c.singleton_class.send(:define_method, :[]) { |b_| b = b_ }
       yield c
-      mergee.merge! binding2hash b
+      mergee.merge! binding_vars(b)
     end
 
-    def binding2hash(b)
-      Hash[b.eval("local_variables").map { |l| [l, b.eval("#{l}")] }]
+    def binding_vars(b)
+      Hash[b.eval("local_variables").reject{|v|v[0,2]=="__"}.map{|l| [l, b.eval("#{l}")] }]
     end
 
-    def theBinding
-      @binding ||= binding
-    end
-
-    def ostructToHash(ostruct, seen = {}.compare_by_identity)
-      return if seen[ostruct]
-      seen[ostruct] = true
-      case ostruct
-      when OpenStruct; ostructToHash ostruct.instance_variable_get(:@table), seen
-      when Hash; Hash[ostruct.select { |k,_| k != :__ }.map { |k,v| v = ostructToHash(v, seen) and [k, v] }.compact]
-      when Array; ostruct.map { |v| ostructToHash(v) }.compact
-      else ostruct
+    def hashize(obj, seen = {}.compare_by_identity)
+      return if seen[obj]
+      seen[obj] = true
+      case obj
+      when OpenStruct; hashize(obj.instance_variable_get(:@table), seen)
+      when Hash; Hash[obj.reject{|k,_|k[0, 2] == "__"}.map { |k,v| v = hashize(v, seen) and [k, v] }.compact]
+      when Array; obj.map { |v| hashize(v, seen) }.compact
+      when Fixnum, Float, true, false, nil; seen[obj] = false; obj
+      else obj
       end
     end
-  end
 
-  def self.load(toml)
-    ctx = LoadContext.new
-    result = ctx.theBinding.eval(to_ruby(toml))
-    ctx.ostructToHash ctx.binding2hash(ctx.theBinding)
-  end
-
-  class << self
-  private
     def to_ruby(toml)
       "if true\n#{toml.gsub(/^\s*\[([a-z0-9_.]+)\]\s*$/i){|sec|" end; #{1.
       upto($1.split(".").length-1).map { |i| "#{$1.split(".")[0...i].join(
